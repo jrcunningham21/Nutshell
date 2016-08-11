b8777221682477da30d8c80d0f30b9830f295d99
jrcunningham21 <jrcunningham21@gmail.com>
2016-08-10 22:25:57 -0500

    Nutshell.Database project added for db schema, Nutshell.Models created for enitity models, 2 tables added FirstTable and User

---
+++ Nutshell.Web/Nutshell.Database/dbo/Tables/FirstTable.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[FirstTable] (
+    [FirstTableId] INT NOT NULL,
+    CONSTRAINT [PK_FirstTable] PRIMARY KEY CLUSTERED ([FirstTableId] ASC)
+);
+
---
+++ Nutshell.Web/Nutshell.Database/dbo/Tables/User.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User] (
+    [UserId]     INT           NOT NULL,
+    [Email]      VARCHAR (250) NULL,
+    [FacebookId] VARCHAR (250) NULL,
+    [Name]       VARCHAR (250) NULL,
+    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([UserId] ASC)
+);
+
